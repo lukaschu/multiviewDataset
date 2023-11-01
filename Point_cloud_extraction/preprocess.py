c0cc89cb3b3e5dbd0ea8094741115385fa80c879
Simon <simschla@student.ethz.ch>
2023-11-01 11:36:59 +0100

    minor changes

--- Point_cloud_extraction/preprocess.py
+++ Point_cloud_extraction/preprocess.py
@@ -6,17 +6,19 @@ import shutil
 # Set the path to your data folder
 data_folder = '/Users/lukasschuepp/framework/hand_data/multiviewDataset/Hand_Data_3500'
 destination_folder = '/Users/lukasschuepp/framework/hand_data/multiviewDataset/Hand_Data_split'
+category_id = '10'
+category = "hand"
 
 # create destination folder
 os.makedirs(destination_folder, exist_ok=True)
 
 # create subfolders
-os.makedirs(os.path.join(destination_folder, 'train', 'complete'), exist_ok=True)
-os.makedirs(os.path.join(destination_folder, 'train', 'partial'), exist_ok=True)
-os.makedirs(os.path.join(destination_folder, 'valid', 'complete'), exist_ok=True)
-os.makedirs(os.path.join(destination_folder, 'valid', 'partial'), exist_ok=True)
-os.makedirs(os.path.join(destination_folder, 'test', 'complete'), exist_ok=True)
-os.makedirs(os.path.join(destination_folder, 'test', 'partial'), exist_ok=True)
+os.makedirs(os.path.join(destination_folder, 'train', 'complete', category_id), exist_ok=True)
+os.makedirs(os.path.join(destination_folder, 'train', 'partial', category_id), exist_ok=True)
+os.makedirs(os.path.join(destination_folder, 'valid', 'complete', category_id), exist_ok=True)
+os.makedirs(os.path.join(destination_folder, 'valid', 'partial', category_id), exist_ok=True)
+os.makedirs(os.path.join(destination_folder, 'test', 'complete', category_id), exist_ok=True)
+os.makedirs(os.path.join(destination_folder, 'test', 'partial', category_id), exist_ok=True)
 
 # Create directories for train, validation, and test splits
 train_folder = os.path.join(destination_folder, 'train')
@@ -48,11 +50,11 @@ test_files = file_list[num_train + num_valid:]
 # Function to copy files to the specified folder
 def copy_files(src_files, dest_folder):
     for file in src_files:
-        shutil.copy(os.path.join(data_folder, 'complete', file), os.path.join(dest_folder, 'complete', file))
+        shutil.copy(os.path.join(data_folder, 'complete', file), os.path.join(dest_folder, 'complete', category_id, file))
         for partial_pc in os.listdir(os.path.join(data_folder, 'partial', file.split('.')[0])):
             # create destination folder
-            os.makedirs(os.path.join(dest_folder, 'partial', file.split('.')[0]), exist_ok=True)
-            shutil.copy(os.path.join(data_folder, 'partial', file.split('.')[0], partial_pc), os.path.join(dest_folder, 'partial', file.split('.')[0], partial_pc))
+            os.makedirs(os.path.join(dest_folder, 'partial', category_id, file.split('.')[0]), exist_ok=True)
+            shutil.copy(os.path.join(data_folder, 'partial', file.split('.')[0], partial_pc), os.path.join(dest_folder, 'partial', category_id, file.split('.')[0], partial_pc))
 
 
 # Copy files to the respective split folders
@@ -62,16 +64,21 @@ copy_files(test_files, test_folder)
 
 # Create a JSON file with the split information
 split_info = [{
-    "taxonomy_id": "10",
-    "taxonomy_name": "hand",
+    "taxonomy_id": category_id,
+    "taxonomy_name": category,
     "train": train_files,
     "valid": valid_files,
     "test": test_files
 }]
 
-json_file_path = os.path.join(data_folder, 'split_info.json')
+json_file_path = os.path.join(destination_folder, 'split_info.json')
 
 with open(json_file_path, 'w') as json_file:
     json.dump(split_info, json_file)
+
+category_file_path = os.path.join(destination_folder, 'category.txt')
+
+with open(category_file_path, 'w') as category_file:
+    category_file.write(category)
 
 print("Data splitting and JSON file creation complete.")
